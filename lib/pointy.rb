e0d802cb1b056e7a550f746c085b502ae6c76b9e
Chris Sloey <chris.sloey@gmail.com>
2014-09-16 11:47:31 +0100

    Pointy.route(from, to) to Retrieve route points from Google Directions API

--- lib/pointy.rb
+++ lib/pointy.rb
@@ -1,5 +1,54 @@
-require "pointy/version"
+require_relative "./pointy/version"
+require "httparty"
+require "polylines"
 
 module Pointy
-  # Your code goes here...
+  include HTTParty
+  base_uri 'https://maps.googleapis.com/maps/api'
+
+  default_params mode: 'walking'
+  default_params sensor: 'false'
+  default_params key: ENV['GOOGLE_DIRECTIONS_API_KEY']
+
+  # from and to should be of format:
+  # {
+  #   lat: float
+  #   long: float
+  # }
+  def self.route(from, to, opts={})
+    # Format points for Google API
+    opts.merge!({
+      origin: "#{from[:lat]},#{from[:long]}",
+      destination: "#{to[:lat]},#{to[:long]}"
+    })
+
+    response = get('/directions/json', query: opts)
+
+    # Check there are routes
+    routes = response['routes']
+    return if routes.nil? or routes.empty?
+
+    # Gather all route points
+    route = routes.first
+    legs = route['legs']
+    return if legs.nil? or legs.empty?
+
+    points = []
+    legs.each do |leg|
+      steps = leg['steps']
+      next unless steps
+      steps.each_with_index do |step, index|
+        polyline = step['polyline']['points']
+        decoded_poly = Polylines::Decoder.decode_polyline(polyline)
+        points += decoded_poly.map do |coord|
+          {
+            lat: coord.first,
+            long: coord.last
+          }
+        end
+      end
+    end
+
+    points
+  end
 end
